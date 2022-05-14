ab81257fb11c6d225c34e4a20c3e570b87592667
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-05-14 00:44:03 -0300

    Fazendo tratamentos de erros e MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -16,12 +18,27 @@ export class CoursesComponent implements OnInit {
 
   // coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe( // o pipe que faz o tratamento de erro
+        catchError (error => { // se quiser fazer o spinner parar precisa retornar dados, mesmo que vázio.
+        this.onError('Erro ao carregar cursos.');
+          return of([])
+        })
+    );
 
     // this.coursesService.list().subscribe(courses => this.courses = courses);
+  }
+
+  onError(errorMsg: string){
+      this.dialog.open(ErrorDialogComponent, {
+        data: errorMsg
+      });
   }
 
   ngOnInit(): void {
