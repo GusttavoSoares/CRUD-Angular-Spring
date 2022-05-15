84960cf8b705766d7d0866430e6617e155923cc5
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-05-15 18:32:54 -0300

    Fazendo o PostMapping para o create de cursos e fazendo o botão cancelar e salvar retornar para a lista de curso quando clicado.

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import {Location} from '@angular/common';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
@@ -13,7 +13,10 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private service: CoursesService, private snackBar: MatSnackBar) {
+  constructor(private formBuilder: FormBuilder,
+  private service: CoursesService,
+  private snackBar: MatSnackBar,
+  private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -26,13 +29,17 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit(){
       this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => this.onError());
+      .subscribe(result => this.onSucess(), error => this.onError());
   }
 
   onCancel(){
-
+    this.location.back();
   }
 
+  private onSucess(){
+    this.snackBar.open('Curso salvo com sucesso!', '',{duration: 3000 });
+    this.onCancel();
+  }
   private onError(){
     this.snackBar.open('Erro ao salvar curso', '',{duration: 3000 });
   }
--- crud-spring/src/main/java/com/ifsp/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/ifsp/crudspring/controller/CourseController.java
@@ -5,9 +5,14 @@ import java.util.List;
 import com.ifsp.crudspring.model.Course;
 import com.ifsp.crudspring.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -24,4 +29,14 @@ public class CourseController {
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
     }
+
+    @PostMapping // O ResponseEntity, além de mandar a resposta ele possibilita mudar o status Http
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){ // Spring recebe o payload do corpo do request e tenta mapear com a category e name do classe Course
+       // System.out.println(course.getName());
+       // return ResponseEntity.status(HttpStatus.CREATED)
+        //.body(courseRepository.save(course));
+        return courseRepository.save(course);
+    }
 }
+
